c673076290073b3431bedb79ae879535b272246d
i-s-d <ivan.didyk.777@gmail.com>
2024-08-17 13:58:15 +0300

    cigue: base of the gui

---
+++ include/cigue/base.h
@@ -0,0 +1,96 @@
+//! 
+//! Основа библиотеки. Здесь написана структура виджетов,
+//! состояние и функции, использующиеся для отрисовки всего дерева.
+//!
+//! Все вещи, которые нужны только один кадр (в т.ч. сами виджеты)
+//! создаются в буффере.
+//!
+
+#pragma once
+#include "cigue/memory.h"
+
+struct cigue_state;
+
+struct cigue_widget {
+  /// Следующий виджет в списке детей какого-то
+  /// родительского виджета.
+  struct cigue_widget* next;
+
+  /// Первый ребёнок этого виджета.
+  /// Следующие за ним можно добыть как `first_child->next->next...`.
+  struct cigue_widget* first_child;
+
+  /// Высота виджета над строкой.
+  /// Если виджет - одна строка текста,
+  /// то он весь над строкой.
+  int above_baseline;
+
+  /// Размеры этого виджета. Ни больше, ни меньше.
+  int width, height;
+
+  /// Местоположение этого виджета на экране.
+  int x, y;
+
+  /// Данная функция считает ширину и высоту этого виджета,
+  /// уже зная всё содержимое.
+  ///
+  /// Можно задать `NULL`, тогда `width` и `height` останется таким же,
+  /// с каким этот виджет был создан.
+  void (*compute_dimensions)(struct cigue_state* state, struct cigue_widget* self);
+
+  /// Считаем позиции детей, зная свою.
+  /// Затем рисуем этот виджет.
+  /// Детей не нужно рисовать, их отрисует `cigue_render()`.
+  ///
+  /// Это последний вызов кадра, так что если в `cigue_state.buf`
+  /// было что-то, что нужно чистить, то это нужно сделать здесь.
+  void (*layout_and_draw)(struct cigue_state* state, struct cigue_widget* self);
+
+  /// Данные этого виджета.
+  /// Ими распоражаются 
+  void* widget_data;
+};
+
+typedef struct cigue_widget cigue_widget;
+
+struct _cigue_widget_stack {
+  struct _cigue_widget_stack* parent;
+  cigue_widget* wgt;
+  cigue_widget** new_child_place;
+};
+
+typedef struct _cigue_widget_stack _cigue_widget_stack;
+
+/// Состояние всего GUI.
+struct cigue_state {
+  /// Буффер, из которого аллоцируется память на виджеты и
+  /// их внутренние свойства.
+  cigue_mem_buffer* buf;
+
+  /// Первый виджет.
+  cigue_widget* _first;
+
+  /// Стек, используемый при `begin/end_widget()`
+  /// Не трогать.
+  /// Аллоцируется в буффере
+  _cigue_widget_stack* _stack_top;
+};
+
+typedef struct cigue_state cigue_state;
+
+/// Создаём объект, в котором данные, которые хранятся дольше одного кадра.
+cigue_state* cigue_new_state();
+
+/// Удаляем этот объект.
+void cigue_free_state(cigue_state* state);
+
+/// Мы построили дерево виджетов, теперь нужно их отрисовать.
+/// После этого состояние подготавливается к новому кадру.
+void cigue_render(cigue_state* state);
+
+/// Начали новый виджет - как открыли HTML-ный тег.
+/// Сам виджет должен быть аллоцирован в буффере.
+void cigue_begin_widget(cigue_state* state, cigue_widget* widget);
+
+/// Закрыли этот виджет.
+void cigue_end_widget(cigue_state* state);
---
+++ src/cigue/base.c
@@ -0,0 +1,77 @@
+#include "cigue/base.h"
+#include "cigue/memory.h"
+#include <stdlib.h>
+#include <assert.h>
+
+cigue_state* cigue_new_state() {
+  cigue_state* st = malloc(sizeof(cigue_state));
+  st->buf = cigue_mem_new_buffer(1024);
+  st->_first = NULL;
+  st->_stack_top = NULL;
+  return st;
+}
+
+void cigue_free_state(cigue_state* state) {
+  cigue_mem_free_buffer(state->buf);
+  free(state);
+}
+
+static void recurse_compute_dims(cigue_state* st, cigue_widget* wgt) {
+  if (wgt->first_child)
+    recurse_compute_dims(st, wgt->first_child);
+  if (wgt->compute_dimensions)
+    wgt->compute_dimensions(st, wgt);
+  if (wgt->next)
+    recurse_compute_dims(st, wgt->next);
+}
+
+static void recurse_render(cigue_state* st, cigue_widget* wgt) {
+  if (wgt->layout_and_draw)
+    wgt->layout_and_draw(st, wgt);
+  if (wgt->first_child)
+    recurse_render(st, wgt->first_child);
+  if (wgt->next)
+    recurse_render(st, wgt->next);
+}
+
+
+void cigue_render(cigue_state* state) {
+  // Сам рендер
+  recurse_compute_dims(state, state->_first);
+  recurse_render(state, state->_first);
+
+  // Подготовка к новому кадру
+  state->_first = NULL;
+  state->_stack_top = NULL;
+  cigue_mem_new_frame(state->buf);
+}
+
+void cigue_begin_widget(cigue_state* state, cigue_widget* widget) {
+  _cigue_widget_stack* stack_item = cigue_mem_alloc(state->buf, sizeof(_cigue_widget_stack));
+  if (state->_first == NULL) {
+    // Первый виджет
+    state->_first = widget;
+    stack_item->parent = NULL;
+  } else if (state->_stack_top == NULL) {
+    // Не первый виджет, но стек пустой.
+    // Получили второй корневой виджет.
+    assert(0 && "There must be only one root widget");
+  } else {
+    // Просто какой-то виджет.
+    *state->_stack_top->new_child_place = widget;
+    stack_item->parent = state->_stack_top;
+  }
+  stack_item->wgt = widget;
+  stack_item->new_child_place = &widget->first_child;
+  state->_stack_top = stack_item;
+}
+
+void cigue_end_widget(cigue_state* state) {
+  assert(state->_stack_top != NULL && "There should be some widget to end");
+  _cigue_widget_stack* item = state->_stack_top;
+  state->_stack_top = state->_stack_top->parent;
+  if (!state->_stack_top) return; // закрыли корневой
+  state->_stack_top->new_child_place = &item->wgt->next;
+  // `item` остаётся в буффере, хотя он уже не нужен.
+  // TODO: переиспользовать их
+}
